24335934f79c0297e12cb08dbff10f1fd6234e53
Jeremias0618 <yeremitantaraico@gmail.com>
2026-05-14 18:24:14 -0500

    Ejercicio 01

---
+++ EP/Ejercicios 01/Ejercicios 01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ejercicios_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el consumo del cliente:");
+            double consumo = double.Parse(Console.ReadLine());
+
+            double propina = CalcularPropina(consumo);
+            double total = CalcularTotal(consumo, propina);
+
+            Console.WriteLine($"Consumo: {consumo}");
+            Console.WriteLine($"Propina: {propina}");
+            Console.WriteLine($"Total a pagar: {total}");
+        }
+
+        static double CalcularPropina(double consumo)
+        {
+            return consumo * 0.10;
+        }
+
+        static double CalcularTotal(double consumo, double propina)
+        {
+            return consumo + propina;
+        }
+    }
+}
